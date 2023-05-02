92ee8660336a2f77b6b3d8e21f4fdbbe7838a5e3
Patriks Raiss-Reiss <64326465+SeetZy@users.noreply.github.com>
2023-05-02 17:54:00 +0300

    some edits

---
+++ app/src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="form-check">
+    <input
+      class="form-check-input"
+      type="radio"
+      name="flexRadioDefault"
+      id="flexRadioDefault1"
+    />
+    <label class="form-check-label" for="flexRadioDefault1">
+      <!-- {{ answer }} -->
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+// export default {
+//   props: {
+//     answer: String,
+//   },
+// }
+</script>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
-document.title = 'QUIZ // DATORIUM KAHOOT'
+/**
+ * * Library imports
+ */
+import { onMounted } from 'vue'
+// ? https://www.npmjs.com/package/gsap
+import gsap from 'gsap'
+
+/**
+ * * Component imports
+ */
+import Form from '../components/Form.vue'
+
+onMounted(() => {
+  document.title = 'QUIZ // DATORIUM QUIZ'
+
+  const tl = gsap.timeline({ default: { duration: 2.5, ease: 'expo.out' } })
+  tl.fromTo(
+    '#form',
+    { y: -50, opacity: 0 },
+    { y: 0, opacity: 1, duration: 0.8 }
+  )
+})
+
+// export default {
+//   components: {
+//     ChildComp,
+//   },
+// }
 </script>
 
 <template>
-  <div class="w-full h-screen bg-red-400 flex justify-center">
+  <div class="w-full h-screen bg-red-400 flex items-center flex-col" id="form">
     <h1>Datorium Quiz</h1>
+    <div class="flex items-center justify-center"></div>
+    <h2>Datubāzes</h2>
+    <div
+      class="flex items-start justify-center flex-col w-2/4 h-1/4 border-2 rounded bg-white"
+    >
+      <h3>Kas ir primārā atslēga?</h3>
+
+      <Form />
+      <Form />
+      <Form />
+      <Form />
+    </div>
   </div>
 </template>
