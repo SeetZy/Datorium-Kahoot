0a3a0a6c716a99c5dd003e8478a9382a7a57c154
Karlis Veckagans <veckaganskarlis@gmail.com>
2023-05-02 20:33:33 +0300

    updated arhitecture Co-authored-by: FTyumin <FTyumin@users.noreply.github.com>

---
+++ app/src/components/questions/datorsys.vue
@@ -0,0 +1,62 @@
+<template>
+   <div class="mt-4 mb-4 p-6 border-2 rounded bg-white">
+    <h2 class="mb-4">Datorsistēmas</h2>
+    <h3 class="mb-3">Ko dara nslookup funkcija?</h3>
+    <form @submit.prevent>
+      <form @submit.prevent>
+      <Form
+        :answer="`Pārbauda, vai tīkla savienojums starp diviem datoriem ir iespējams`"
+      />
+      <Form
+        :answer="`Kas palīdz meklēt informāciju par datu bāzes objektiem`"
+      />
+      <Form
+        :answer="`Ļauj noteikt, vai dators ir savienots ar internetu un noskaidrot tā IP adresi`"
+      />
+      <Form
+        :answer="`Ļauj automātiski atlasīt un konfigurēt tīkla savienojumus starp vairākiem datoriem un tīkla elementiem`"
+      />
+
+    </form>
+    <h3 class="mb-3">Kas ir IP adrese?</h3>
+    <form @submit.prevent>
+      <Form
+        :answer="`Programmatūras veids, ko izmanto datu bāzu pārvaldīšanai`"
+      />
+      <Form
+        :answer="`Unikāls skaitliskais identifikators, kas piešķirts katrai ierīcei tīklā, kas izmanto internetu`"
+      />
+      <Form
+        :answer="`Fiziskā adrese, ko tīkla ierīces izmanto, lai sazinātos savā starpā`"
+      />
+      <Form
+        :answer="`Tīmekļa pārlūkprogrammas veids, ko izmanto, lai piekļūtu internetam`"
+      />
+    </form>
+
+    <h3 class="mb-3">Kas ir HTTPS?</h3>
+    <form @submit.prevent>
+      <Form
+        :answer="`Drošs protokols, ko izmanto datu pārsūtīšanai starp tīmekļa pārlūkprogrammu un tīmekļa serveri`"
+      />
+      <Form
+        :answer="`Rīks, ko izmanto datu bāzu pārvaldībai`"
+      />
+      <Form
+        :answer="`Programmēšanas valoda, ko izmanto web lapu veidošanai`"
+      />
+      <Form
+        :answer="`API`"
+      />
+    </form>
+    <button type="submit" class="btn btn-primary mt-2">Iesniegt</button>
+  </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+/**
+ * * Component imports
+ */
+import Form from '.././Form.vue'
+</script>
---
+++ app/src/components/questions/problemAnalysis.vue
@@ -0,0 +1,58 @@
+<template>
+   <div class="mt-4 mb-4 p-6 border-2 rounded bg-white">
+    <h2 class="mb-4">Problēmas analīze</h2>
+    <h3 class="mb-3">Kas ir SWOT analīze?</h3>
+
+    <form>
+    <form>
+      <Form
+        :answer="`Analīzes metode, kas koncentrējas uz spēcīgo un vājo pušu noteikšanu`"
+      />
+      <Form
+        :answer="`Analīzes metode, kas pēta uzņēmuma darba vides ietekmi`"
+      />
+      <Form :answer="`Analīzes metode, kas pēta uzņēmuma finanšu stāvokli`" />
+      <Form
+        :answer="`Analīzes metode, kas koncentrējas uz uzņēmuma tehnoloģisko attīstību`"
+      />
+    </form>
+    <h3 class="mb-3">Kas ir SWOT analīze?</h3>
+
+    <form>
+      <Form
+        :answer="`Analīzes metode, kas koncentrējas uz spēcīgo un vājo pušu noteikšanu`"
+      />
+      <Form
+        :answer="`Analīzes metode, kas pēta uzņēmuma darba vides ietekmi`"
+      />
+      <Form :answer="`Analīzes metode, kas pēta uzņēmuma finanšu stāvokli`" />
+      <Form
+        :answer="`Analīzes metode, kas koncentrējas uz uzņēmuma tehnoloģisko attīstību`"
+      />
+    </form>
+    <h3 class="mb-3">Kas ir SWOT analīze?</h3>
+
+    <form>
+      <Form
+        :answer="`Analīzes metode, kas koncentrējas uz spēcīgo un vājo pušu noteikšanu`"
+      />
+      <Form
+        :answer="`Analīzes metode, kas pēta uzņēmuma darba vides ietekmi`"
+      />
+      <Form :answer="`Analīzes metode, kas pēta uzņēmuma finanšu stāvokli`" />
+      <Form
+        :answer="`Analīzes metode, kas koncentrējas uz uzņēmuma tehnoloģisko attīstību`"
+      />
+    </form>
+    <button type="submit" class="btn btn-primary mt-2">Iesniegt</button>
+  </form>
+    
+  </div>
+</template>
+
+<script setup lang="ts">
+/**
+ * * Component imports
+ */
+ import Form from '.././Form.vue'
+</script>
